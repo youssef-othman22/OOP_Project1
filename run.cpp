23db3ad188ebc0da64142518da235d47a3569414
mahmoudnasser1561@gmail.com <mahmoudnasser1561@gmail.com>
2024-12-08 16:55:20 +0200

    runner

--- run.cpp
+++ run.cpp
@@ -1,3 +1,15 @@
-/*
-  run the code here
-*/
+#include <iostream>
+#include "student/Student.h"
+#include "professor/Professor.h"
+#include "subject/Subject.h"
+
+void displayMenu() {
+    std::cout << "School Management System\n";
+    std::cout << "1. Add a new Student\n";
+    std::cout << "2. Assign a Subject to a Student\n";
+    std::cout << "3. View all Professors\n";
+    std::cout << "4. Mark Attendance\n";
+    std::cout << "5. View Grades\n";
+    std::cout << "6. Exit\n";
+    std::cout << "Enter your choice: ";
+}
