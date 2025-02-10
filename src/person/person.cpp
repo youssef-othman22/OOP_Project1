45f6323e8798aeefa3f137d4bd41b773b5576721
Youssef Othman <youssefcourses00l@gmail.com>
2025-02-10 23:55:56 +0200

    i implemented person.cpp file and add string library

--- src/person/person.cpp
+++ src/person/person.cpp
@@ -1,23 +1,24 @@
 #include "person.h"
-
+#include <string>
 Person::Person(std::string name, int age) {
-
+this->name = name ;
+this->age = age ;
 }
 
 int Person::getAge() {
-
+return age ;
 }
 
 std::string Person::getName() {
-
+return name ;
 }
 
 void Person::setAge(int age) {
-
+this->age = age ;
 }
 
 void Person::setName(std::string name) {
-  
+  this->name = name ;
 }
 
 
